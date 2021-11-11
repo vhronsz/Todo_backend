f6f83396de2f532446c67dbded16e1b42687fefa
Ryan Sanjaya <ryansanjaya290799@gmail.com>
2021-11-11 14:50:01 +0700

    Change port to 3001

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.json({
     data : "Hello World"
   });
