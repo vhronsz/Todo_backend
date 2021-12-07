9de3082c681bec5c0496118df2b683dddadb9e16
Ryan Sanjaya <ryansanjaya290799@gmail.com>
2021-12-07 15:30:51 +0700

    Add sanitize input and login functionality

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../tools/db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,11 +11,32 @@ router.get('/', function(req, res, next) {
 
 router.post('/login', function(req, res, next) {
   // console.log(req.body.username);
-  
-  res.json({
-    status:true,
-    message:"Your credential is invalid"
-  });  
+  let userData = {
+    name : encodeURIComponent(req.body.username.trim()),
+    password:encodeURIComponent(req.body.password.trim())
+  };
+
+  db.query(`Select password from users where name like "${userData.name}"`,function(err,rows,fields){
+    if(err) throw err;
+    if(rows.length < 1){
+      res.json({
+        status:true,
+        message:"User not exist"
+      });
+    }
+    if(rows[0].password === userData.password){
+      res.json({
+        status:true,
+        message:"Redirecting"
+      });
+    }
+    else {
+      res.json({
+        status:false,
+        message : "Your credential is wrong"
+      });  
+    }
+  });
 });
 
 module.exports = router;
--- routes/item.js
+++ routes/item.js
@@ -1,6 +1,17 @@
 var express = require('express');
-var db = require('../tools/db');
+var db = require("../tools/db");
 var router = express.Router();
+
+router.get("/",function(req,res,next){
+    let user = req.body.user;
+
+    db.query
+    (
+        `select * from sections join users on sections.user_id = users.id`+
+        `where user.id like ${user.id}`,function(err,rows,fields){
+
+        });
+});
 
 router.post("/add",function(req,res,next){
     res.json({
@@ -12,11 +23,6 @@ router.post("/add",function(req,res,next){
 });
 
 router.post("/check",function(req,res,next){
-    db.query("Select * from users",function(err,rows,fields){
-        if(err) throw err;
-        console.log(rows);
-    });
-    
     res.json({
       status:true,
       message:"Task Complete"
