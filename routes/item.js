140723b527ccf1c0a7e32c6eb9e1c28938a49de8
Ryan Sanjaya <ryansanjaya290799@gmail.com>
2021-12-09 23:46:43 +0700

    Fetch action data from database

--- routes/item.js
+++ routes/item.js
@@ -7,13 +7,21 @@ router.get("/",function(req,res,next){
 
     db.query
     (
-        `SELECT action.name as action,sections.id,users.name FROM action join sections on action.section_id = sections.id join users on sections.user_id = users.id 
-        where users.id = ${user.id}`,
+        `SELECT action.name as action,sections.id,sections.name FROM action join sections on action.section_id = sections.id join users on sections.user_id = users.id 
+        where users.id = ${user.id} order by sections.id desc`,
         function(err,rows,fields){
             if(err)throw err;
-            console.log(rows);
+            let data = [];
+            let size = rows[0].id;
+
+            for(let i = size;i>0;i--){
+                data.push(rows.filter((row)=>{
+                    return row.id === i;
+                }));
+            }
+
             res.json({
-                rows
+                "data":data
             });
         }
     );
